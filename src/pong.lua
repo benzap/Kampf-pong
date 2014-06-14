a5456994afc4fb451a06ab9181107a711fb3b569
Benjamin Zaporzan <btzaporz@lakeheadu.ca>
2014-06-14 18:56:40 -0400

    Almost finished pong clone

--- src/pong.lua
+++ src/pong.lua
@@ -3,6 +3,28 @@ local func = require "engine/func"
 local sprite = require "engine/sprite"
 local events = require "engine/events"
 local text = require "engine/text"
+local timers = require "engine/timers"
+
+-- CONFIGURATION SETTINGS
+controls = {
+	player_one = {
+		padel_up = "w",
+		padel_down = "s",
+	},
+	player_two = {
+		padel_up = "up",
+		padel_down = "down",
+	},
+	reset = "r",
+	easteregg = "insert",
+}
+
+config = {
+	padelVelocity = 200,
+	ballSpeed = {200, 100},
+	ballSpeedIncrement = 10,
+	resetDelay = 2000,
+}
 
 -- WINDOW SETTINGS
 local renderWindow = kf.getRenderWindow()
@@ -108,10 +130,64 @@ local ball = sprite:new{
 	},
 	collision = {
 		type = "AABB",
-		width = 20,
-		height = 20,
+		width = ballBounds.right,
+		height = ballBounds.bottom,
 	},
 }
+
+function ball:reset(delay, bLeft)
+	delay = delay or 0
+
+	self:setPosition{
+		gameBounds.right / 2 - ballBounds.right / 2 ,
+		gameBounds.top  + 
+			(gameBounds.bottom - gameBounds.top) / 2 - 
+			ballBounds.bottom / 2
+	}
+
+	ball:setVelocity(0,0)
+
+	timerCallback = function()
+		if bLeft then
+			self:bounceLeft()
+			self:bounceUp()
+		else
+			self:bounceRight()
+			self:bounceDown()
+		end
+	end
+
+	if delay > 0 then
+		timers.createEventTimer(delay, timerCallback)
+	else
+		timerCallback()
+	end
+end
+
+function ball:bounceLeft()
+	local velocity = ball:getVelocity()
+	velocity[1] = -config.ballSpeed[1]
+	ball:setVelocity(velocity)
+end
+
+function ball:bounceRight()
+	local velocity = ball:getVelocity()
+	velocity[1] = config.ballSpeed[1]
+	ball:setVelocity(velocity)
+end
+
+function ball:bounceUp()
+	local velocity = ball:getVelocity()
+	velocity[2] = -config.ballSpeed[2]
+	ball:setVelocity(velocity)
+end
+
+function ball:bounceDown()
+	local velocity = ball:getVelocity()
+	velocity[2] = config.ballSpeed[2]
+	ball:setVelocity(velocity)
+end
+
 
 local text_left = text:new{
 	name = "text-scoreboard-left",
@@ -131,11 +207,159 @@ local text_right = text:new{
 	},
 }
 
+--scoreboard functions
+local playerOneScore = 0
+function text_left:inc()
+	playerOneScore = playerOneScore + 1
+	text_left:setText(tostring(playerOneScore))
+end
+
+local playerTwoScore = 0
+function text_right:inc()
+	playerTwoScore = playerTwoScore + 1
+	text_right:setText(tostring(playerTwoScore))
+end
+
+function resetScore()
+	playerOneScore = 0
+	text_left:setText("0")
+	playerTwoScore = 0
+	text_right:setText("0")
+end
+
 -- EVENTS
+events.createKeyListener(
+	function(key, bKeyDown) 
+
+		--Player One
+		if key == controls.player_one.padel_up then
+			if bKeyDown then
+				leftPadel:setVelocity(0, -config.padelVelocity)
+			else
+				leftPadel:setVelocity(0, 0)
+			end
+		elseif key == controls.player_one.padel_down then
+			if bKeyDown then
+				leftPadel:setVelocity(0, config.padelVelocity)
+			else
+				leftPadel:setVelocity(0, 0)
+			end
+		end
+
+		--Player Two
+		if key == controls.player_two.padel_up then
+			if bKeyDown then
+				rightPadel:setVelocity(0, -config.padelVelocity)
+			else
+				rightPadel:setVelocity(0, 0)
+			end
+		elseif key == controls.player_two.padel_down then
+			if bKeyDown then
+				rightPadel:setVelocity(0, config.padelVelocity)
+			else
+				rightPadel:setVelocity(0, 0)
+			end
+		end
+
+		if key == "space" and bKeyDown then
+			resetGame()
+		end
+
+end)
+
+function startGame()
+	ball:bounceUp()
+	ball:bounceLeft()
+end
+
+function pauseGame()
+	
+end
+
+function resetGame()
+	resetScore()
+	ball:reset(config.resetDelay, true)
+end
+
+--new system for ball bounds
+kf.addSystem(
+	"ball-bounds",
+	function() end,
+	function() 
+		local position = ball:getPosition()
+		if position[1] <= gameBounds.left then
+			ball:reset(config.resetDelay, false)
+			text_left:inc()
+		elseif position[1] + ballBounds.right > gameBounds.right then
+			ball:reset(config.resetDelay, true)
+			text_right:inc()
+		end
+
+		if position[2] <= gameBounds.top then
+			ball:bounceDown()
+		elseif position[2] + ballBounds.bottom > gameBounds.bottom then
+			ball:bounceUp()
+		end
 
 
+end)
+
+--new system for padel bounds
+kf.addSystem(
+	"padel-bounds",
+	function() end,
+	function()
+		function bounds(padel)
+			local position = padel:getPosition()
+
+			if position[2] <= gameBounds.top then
+				position[2] = gameBounds.top
+			elseif position[2] + padelBounds.bottom > gameBounds.bottom then
+				position[2] = gameBounds.bottom - padelBounds.bottom
+			end
+
+			padel:setPosition(position)
+		end
+		
+		bounds(leftPadel)
+		bounds(rightPadel)
+end)
 
 
+--collision listener for the ball
+events.createCollisionListener(
+	ball:getName(),
+	--in
+	function() 
+	
+	end,
+	--out
+	function() 
+		
+end)
+
+--collision listener for the left padel
+events.createCollisionListener(
+	leftPadel:getName(),
+	function(first, second, args)
+		local padel = sprite:get(first)
+		if second:getName() ~= ball:getName() then return end
+		ball:bounceRight()
+	end,
+	function() 
+end)
+
+--collision listener for the left padel
+events.createCollisionListener(
+	rightPadel:getName(),
+	function(first, second, args)
+		local padel = sprite:get(first)
+		if second:getName() ~= ball:getName() then return end
+		ball:bounceLeft()
+	end,
+	function() 
+end)
 
 
+startGame()
 kf.runMainLoop()
